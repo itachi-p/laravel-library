192e6726226306732d8a794917301bfe4e007e88
itachi-p <itachip38@gmail.com>
2024-09-28 20:16:53 +0900

    feat: display 'ANONYMOUS' for books without an author and add navigation links for Authors and Books

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -10,7 +10,12 @@
             </div>
             <div class="col-9">
                 <h1>{{ $book->title }}</h1>
-                <p class="h6 fw-bold">by {{ $book->author->name }}</p>
+                {{-- authorがnullの場合、著者名にはANONYMOUSを表示 --}}
+                @if ($book->author)
+                    <p class="h6 fw-bold">by {{ $book->author->name }}</p>
+                @else
+                    <p class="h6 fw-bold text-muted">by ANONYMOUS</p>
+                @endif
 
                 <p class="text-muted">Published in {{ $book->year_published }}</p>
             </div>
